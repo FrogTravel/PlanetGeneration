2fefc337cd5e0a5f7d7865a39a1890f2eeefa6eb
FrogTravel <e.levchenko@innopolis.ru>
2019-11-10 15:16:27 +0300

    Initial commit

---
+++ Assets/ShapeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator   {
+	ShapeSettings settings;
+	INoiseFilter[] noiseFilter;
+	public MinMax elevationMinMax;
+
+	public void UpdateSettings(ShapeSettings _settings){
+		settings = _settings;
+		noiseFilter = new INoiseFilter[settings.noiseLayers.Length];
+
+		for (int i = 0; i < noiseFilter.Length; i++) {
+			noiseFilter [i] = NoiseFilterFactory.CreateNoiseFilter (settings.noiseLayers [i].noiseSettings);
+		}
+
+		elevationMinMax = new MinMax();
+	}
+
+	public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere){
+		float firstLayerValue = 0;
+		float elevation = 0;
+
+		if (noiseFilter.Length > 0) {
+			firstLayerValue = noiseFilter [0].Evaluate (pointOnUnitSphere);
+			if (settings.noiseLayers [0].enables) {
+				elevation = firstLayerValue;
+			}
+		}
+		for (int i = 1; i < noiseFilter.Length; i++) {
+			if (settings.noiseLayers [i].enables) {
+				float mask = (settings.noiseLayers[i].useFirstLayerAsMask ? firstLayerValue : 1);
+				elevation += noiseFilter [i].Evaluate (pointOnUnitSphere) * mask;
+			}
+		}
+		elevation = settings.planetRadius * (1+elevation);
+		elevationMinMax.AddValue(elevation);
+		return pointOnUnitSphere * elevation;
+	}
+}
---
+++ Assets/ShapeSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ShapeSettings : ScriptableObject {
+	public float planetRadius = 1;
+	public NoiseLayer[] noiseLayers;
+
+	[System.Serializable]
+	public class NoiseLayer{
+		public bool enables = true;
+		public bool useFirstLayerAsMask;
+		public NoiseSettings noiseSettings;
+	}
+}
